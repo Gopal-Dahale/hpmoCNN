bd3705b2be19bf63eab23f276f5420219e5ae698
Gopal-Dahale <dahalegopal27@gmail.com>
2021-10-30 18:41:14 +0530

    remove unused code from cnn neural net

--- cnn/src/neural_net.cu
+++ cnn/src/neural_net.cu
@@ -3,7 +3,4 @@
 NeuralNet::NeuralNet(vector<LayerSpecifier> &layers, DataType data_type, int batch_size,
                      TensorFormat tensor_format, float softmax_eps, float init_std_dev)
 
-{
-  cudaStreamCreate(&stream_compute);
-  cudnn cudaStreamCreate(&stream_memory);
-}
+{}
