1a6f6c3aec4c813dbb808a08c552a55906071b17
Gopal-Dahale <dahalegopal27@gmail.com>
2021-11-05 10:07:09 +0530

    increase lr

--- basic/src/main.cu
+++ basic/src/main.cu
@@ -289,7 +289,7 @@ int main(int argc, char *argv[])
                 softmax_eps, init_std_dev, SGD);
 
   int num_epoch = 1000;
-  double learning_rate = 1e-4;
+  double learning_rate = 1e-1;
   double learning_rate_decay = 0.9;
 
   Solver solver(&net, (void *)f_train_images, f_train_labels,
