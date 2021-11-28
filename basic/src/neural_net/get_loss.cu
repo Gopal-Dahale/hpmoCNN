af6fe9d0df2a094b1b0dd4471089c7e1764a3fcf
Dhruv88 <55611035+Dhruv88@users.noreply.github.com>
2021-11-28 08:34:18 +0530

    Update get_loss.cu

--- basic/src/neural_net/get_loss.cu
+++ basic/src/neural_net/get_loss.cu
@@ -201,6 +201,12 @@ void NeuralNet::getLoss(void *X, int *y, double learning_rate,
       {
         dlayer_input[i] = dlayer_input[i + 1];
       }
+      else
+      {
+        int device = -1;
+        cudaGetDevice(&device);
+        cudaMemPrefetcg=hAsync(&layer_input[i-1],layer_input_size[i-1],device,stream_compute);
+      }
     }
     if (layer_type[i] == CONV)
     {
@@ -332,5 +338,6 @@ void NeuralNet::getLoss(void *X, int *y, double learning_rate,
       continue;
     }
     cudaStreamSynchronize(stream_compute);
+    cudaStreamSynchronize(stream_memory);
   }
 }
