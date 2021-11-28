81776b79f378e4292dcc2da255301a8fd91b1101
Gopal Ramesh Dahale <49199003+Gopal-Dahale@users.noreply.github.com>
2021-11-28 16:31:56 +0530

    change cout to std::cout

--- basic/src/neural_net/get_loss.cu
+++ basic/src/neural_net/get_loss.cu
@@ -43,7 +43,7 @@ void NeuralNet::getLoss(void *X, int *y, double learning_rate,
   double Dalpha = 1.0, Dbeta = 0.0;
 
   // Forward Propagation
-  cout << "Forward Propagation: "<< '\n';
+  std::cout << "Forward Propagation: "<< '\n';
   for (int i = 0; i < num_layers; i++)
   {
     if (train == false && i == num_layers - 1)
@@ -52,10 +52,10 @@ void NeuralNet::getLoss(void *X, int *y, double learning_rate,
     if(i>1 && train == true)
     {
       cudaMemGetInfo(&free_bytes, &total_bytes);
-      cout << "Before Offload: " << free_bytes <<'\n'; 
+      std::cout << "Before Offload: " << free_bytes <<'\n'; 
       cudaMemPrefetchAsync(layer_input[i-1], layer_input_size[i-1]*data_type_size, cudaCpuDeviceId, stream_memory);
       cudaMemGetInfo(&free_bytes, &total_bytes);
-      cout << "After Offload: " << free_bytes <<'\n';
+      std::cout << "After Offload: " << free_bytes <<'\n';
     }
 
     if (layer_type[i] == CONV)
@@ -205,7 +205,7 @@ void NeuralNet::getLoss(void *X, int *y, double learning_rate,
     }
   }
 
-  cout << "Backward Propagation: " << '\n';
+  std::cout << "Backward Propagation: " << '\n';
   for (int i = num_layers - 1; i >= 0; i--)
   {
     if (i > 0)
@@ -217,12 +217,12 @@ void NeuralNet::getLoss(void *X, int *y, double learning_rate,
       else
       {
         cudaMemGetInfo(&free_bytes, &total_bytes);
-        cout << "Before Prefetch: " << free_bytes <<'\n';
+        std::cout << "Before Prefetch: " << free_bytes <<'\n';
         int device = -1;
         cudaGetDevice(&device);
         cudaMemPrefetchAsync(layer_input[i-1],layer_input_size[i-1]*data_type_size,device,stream_memory);
         cudaMemGetInfo(&free_bytes, &total_bytes);
-        cout << "After Prefetch: "<< free_bytes <<'\n';
+        std::cout << "After Prefetch: "<< free_bytes <<'\n';
       }
     }
     if (layer_type[i] == CONV)
@@ -357,7 +357,7 @@ void NeuralNet::getLoss(void *X, int *y, double learning_rate,
     cudaStreamSynchronize(stream_compute);
 
     cudaMemGetInfo(&free_bytes, &total_bytes);
-    cout << "Before Offload (layer and dlayer): "<< free_bytes <<'\n';
+    std::cout << "Before Offload (layer and dlayer): "<< free_bytes <<'\n';
 
     cudaMemPrefetchAsync(layer_input[i+1], layer_input_size[i+1]*data_type_size, cudaCpuDeviceId, stream_memory);
     cudaMemPrefetchAsync(dlayer_input[i+1], layer_input_size[i+1]*data_type_size, cudaCpuDeviceId, stream_memory);
@@ -367,6 +367,6 @@ void NeuralNet::getLoss(void *X, int *y, double learning_rate,
       cudaFree(layer_input[i]);
     
     cudaMemGetInfo(&free_bytes, &total_bytes);
-    cout << "After Offload (layer and dlayer): " << free_bytes<<'\n';
+    std::cout << "After Offload (layer and dlayer): " << free_bytes<<'\n';
   }
 }
