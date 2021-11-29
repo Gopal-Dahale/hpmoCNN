69e0cc8952563fbcb648522d74c9193f80c75d97
Dhruv88 <55611035+Dhruv88@users.noreply.github.com>
2021-11-29 07:19:27 +0530

    Update main.cu

--- basic/src/main.cu
+++ basic/src/main.cu
@@ -16,7 +16,7 @@ using namespace std;
 
 typedef unsigned char uchar;
 
-int num_train = 1500, num_test = 500;
+int num_train = 1000, num_test = 500;
 
 int reverseInt(int n)
 {
@@ -602,13 +602,13 @@ int main(int argc, char *argv[])
 //     layer_specifier.push_back(temp);
 //   }
 
-  int batch_size = 128;
+  int batch_size = 64;
   float softmax_eps = 1e-8;
   float init_std_dev = 0.01;
   NeuralNet net(layer_specifier, DATA_FLOAT, batch_size, TENSOR_NCHW,
                 softmax_eps, init_std_dev, SGD);
 
-  int num_epoch = 15;
+  int num_epoch = 100;
   double learning_rate = 1e-4;
   double learning_rate_decay = 1;
 
