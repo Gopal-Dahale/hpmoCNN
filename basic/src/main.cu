ebe73efad81de250127f5b85143e6df2ad8f61b6
Dhruv <dhruva4000@gmail.com>
2022-01-06 16:27:30 +0530

    bug in train function call in main.cu fixed

--- basic/src/main.cu
+++ basic/src/main.cu
@@ -296,7 +296,8 @@ int main(int argc, char *argv[]) {
                 learning_rate, learning_rate_decay, num_train, num_train);
   vector<float> loss;
   vector<int> val_acc;
-  solver.train(loss, val_acc, doo);
+  vector<float> batch_times;
+  solver.train(loss, val_acc, batch_times, doo);
   int num_correct;
   solver.checkAccuracy(f_train_images, f_train_labels, num_train, &num_correct);
   std::cout << "TRAIN NUM CORRECT:" << num_correct << endl;
