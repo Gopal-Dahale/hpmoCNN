2f81dabb2e075d4a8aea039f3cb8e98edffeced6
Dhruv88 <dhruva4000@gmail.com>
2022-01-14 01:17:15 +0530

    logging for offloading

--- basic/src/neural_net/get_loss.cu
+++ basic/src/neural_net/get_loss.cu
@@ -92,7 +92,7 @@ void NeuralNet::getLoss(void *X, int *y, double learning_rate,
 
     if ((i + 2 < num_layers) && (free_memory <= layer_size)) {
       // logfile << "GPU memory is low, offloading to CPU" << std::endl;
-      // std::cout << (free_bytes - buffer_bytes - buffer_bytes /*(reserved_memory/(i+1))*/) / float(buffer_bytes) << " <= "<< layer_input_size[i + 2] * data_type_size /float(buffer_bytes)<< '\n';
+      std::cout << (free_bytes - buffer_bytes - buffer_bytes /*(reserved_memory/(i+1))*/) / float(buffer_bytes) << " <= "<< layer_input_size[i + 2] * data_type_size /float(buffer_bytes)<< '\n';
 
       /************* Heap logic with workspace fix ********************/
 
@@ -114,14 +114,14 @@ void NeuralNet::getLoss(void *X, int *y, double learning_rate,
       {
         int temp = layer_input_pq.top().second;  // Get the layer index on top
                                                  // of the heap
-        // logfile << "Layer to offload: " << temp << std::endl;
-        // logfile << "Size of the layer to offload: "<< layer_input_pq.top().first * data_type_size /float(buffer_bytes)<< std::endl;
+        std::cout << "Layer to offload: " << temp << std::endl;
+        std::cout << "Size of the layer to offload: "<< layer_input_pq.top().first * data_type_size /float(buffer_bytes)<< std::endl;
         free_layer.push_back(temp);  // Add the layer index to the free layer
                                      // vector
 
         // Update the free bytes
         temp_free_bytes += layer_input_pq.top().first * data_type_size;
-        // std::cout << "Free gigabytes in GPU: "<< temp_free_bytes / float(buffer_bytes) << std::endl;
+        std::cout << "Free gigabytes in GPU: "<< temp_free_bytes / float(buffer_bytes) << std::endl;
         offloaded[temp] = true;  // Mark the layer as offloaded
 
         // Copy the layer to host
